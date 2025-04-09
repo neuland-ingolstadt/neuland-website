748d309ae6fa194d477106fa91e621074bcc8486
Robert Eggl <github@roberteggl.dev>
2025-04-09 23:53:14 +0200

    fix: adjust opacity calculation for cell rendering in GameOfLife component

--- src/app/blog/tags/[tag]/page.tsx
+++ src/app/blog/tags/[tag]/page.tsx
@@ -19,6 +19,7 @@ export async function generateMetadata(props: { params: any }) {
 
 export async function generateStaticParams() {
 	const allTags = await getTags()
+	console.log(allTags)
 	return [...new Set(allTags)].map((tag) => ({ tag }))
 }
 
